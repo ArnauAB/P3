41c034a9f23c851f2f3743d830cbfff9c311af62
Adrián López Pardina <adrian.lopez.pardina@estudiantat.upc.edu>
2024-12-06 18:44:30 +0100

    canvis2

--- src/get_pitch/get_pitch.cpp
+++ src/get_pitch/get_pitch.cpp
@@ -25,9 +25,10 @@ Usage:
     get_pitch --version
 
 Options:
-    --llindar-pot FLOAT   Decision Threshold for power
-    --llindar-r1 FLOAT    Decision Threshold for r1
-    --llindar-rmax FLOAT  Decision Threshold for rmax
+    --llindar-pot FLOAT   Decision Threshold for power [default: -45]
+    --llindar-r1 FLOAT    Decision Threshold for r1 [default: 0.55]
+    --llindar-rmax FLOAT  Decision Threshold for rmax [default: 0.38]
+    --thresh_cclip FLOAT  Decision Threshold for cclip [default: 0.0005]
     -h, --help            Show this screen
     --version             Show the version of the project
 
@@ -52,6 +53,7 @@ int main(int argc, const char *argv[]) {
   float llindar_pot = stof(args["--llindar-pot"].asString());
   float llindar_r1 = stof(args["--llindar-r1"].asString());
   float llindar_rmax = stof(args["--llindar-rmax"].asString());
+  float llindar_cclip = stof(args["--llindar-cclip"].asString());
 
   // Read input sound file
   unsigned int rate;
@@ -70,7 +72,13 @@ int main(int argc, const char *argv[]) {
   /// \TODO
   /// Preprocess the input signal in order to ease pitch estimation. For instance,
   /// central-clipping or low pass filtering may be used.
-  
+  float max = *std::max_element(x.begin(), x.end());
+  for(int i = 0; i < (int)x.size(); i++) {
+    if(abs(x[i]) < thresh_cclip*max) {
+      x[i] = 0.0F;
+    } 
+  }
+
   // Iterate for each frame and save values in f0 vector
   vector<float>::iterator iX;
   vector<float> f0;
@@ -82,7 +90,26 @@ int main(int argc, const char *argv[]) {
   /// \TODO
   /// Postprocess the estimation in order to supress errors. For instance, a median filter
   /// or time-warping may be used.
+  #if 1
+  for (unsigned int i = 1; i < f0.size() - 1; i++)
+  {
+    vector<float> vector(3);
+    vector[0] = f0[i];
+    vector[1] = f0[i+1];
+    vector[2] = f0[i-1];
 
+    if (vector[1] < vector[0])
+      swap(vector[0], vector[1]);
+
+    if (vector[2] < vector[1]){
+      swap(vector[1], vector[2]);
+      if (vector[1] < vector[0])
+        swap(vector[1], vector[0]);
+    }
+    f0[i] = vector[1];
+  }
+  #endif
+  
   // Write f0 contour into the output file
   ofstream os(output_txt);
   if (!os.good()) {
